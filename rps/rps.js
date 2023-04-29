dfcbbee0523a6e3c45de321f59b18890648c8e3f
Gregory Pappas <gnpappas0@gmail.com>
2023-04-28 14:12:38 -1000

    Complete game logic for buttons

--- rps/rps.js
+++ rps/rps.js
@@ -1,10 +1,12 @@
+//? computer player logic based on using random number up to 3 starting with 1
+//? random number tied to string choices
 let getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3) + 1;
     if (choice === 1) {
         return "rock";
     } else if (choice === 2) {
         return "paper";
-    } else {
+    } else if (choice === 3) {
         return "scissors";
     }
 };
@@ -15,7 +17,6 @@ var cpu = 0;
 //? player parameter === button click set to appropriate string input
 //? computer parameter === getComputerChoice function
 let playRound = (player, computer) => {
-    player = player.toLowerCase();
     //?game logic w/ point tallying
     if (player === "rock") {
         if (computer === "rock") {
@@ -23,7 +24,7 @@ let playRound = (player, computer) => {
         } else if (computer === "scissors") {
             human++;
             return "Player wins! Rock crushes scissors!";
-        } else {
+        } else if (computer === "paper") {
             cpu++;
             return "Computer wins! Paper suffocates rock!";
         }
@@ -34,7 +35,7 @@ let playRound = (player, computer) => {
         } else if (computer === "rock") {
             cpu++;
             return "Computer wins! Rock crushes scissors!";
-        } else {
+        } else if (computer === "paper") {
             human++;
             return "Player Wins! Scissors cuts paper!";
         }
@@ -45,34 +46,60 @@ let playRound = (player, computer) => {
         } else if (computer === "rock") {
             human++;
             return "Player wins! Paper suffocates rock!";
-        } else {
+        } else if (computer === "scissors") {
             cpu++;
             return "Computer wins! Scissors cuts paper!";
         }
     }
-    //?when a player or players reach 5 points, return results and end game
+};
+
+let result = () => {
+    //?when a player or players reach 5 points, return results
     if (human === 5 && cpu < 5) {
         alert(
             `Player wins with a score of ${human}!\nFinal score: Player: ${human} | Computer: ${cpu}.`
         );
     } else if (human === 5 && cpu === 5) {
         alert(`It's a tie!\nFinal score: Player: ${human} | Computer: ${cpu}.`);
-    } else if (human < 5 && cpu === 5){
+    } else if (human < 5 && cpu === 5) {
         alert(
             `Computer wins with a score of ${cpu}!\nFinal score: Computer: ${cpu} | Player: ${human}.`
         );
     }
 };
-//? variables set to the buttons' class in index.HTML
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissors = document.querySelector('scissors');
 
-//? set event listeners to click to run playRound function with appropriate string input
+//? variables are set to the buttons' class in index.HTML
 
-rock.addEventListener('click', playRound('rock', getComputerChoice));
-paper.addEventListener('click', playRound('paper', getComputerChoice));
-scissors.addEventListener('click', playRound('scissors', getComputerChoice));
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const body = document.querySelector("body");
+const announcer = document.createElement('div');
+
+//? class assigning
+announcer.classList.toggle('announcer');
+
+//? announcer box to show round results and end results after five rounds
+
+
+/* EVENT LISTENERS
+? set event listeners to click to run empty function that runs the game and results functions
+? event listeners return each result to a string that is wrapped in <p>, and attached to a div to show a running list of each return statement  
+ */
+
+rock.addEventListener("click", function () {
+    console.log(playRound("rock", getComputerChoice()));
+    result();
+});
+paper.addEventListener("click", function () {
+    console.log(playRound("paper", getComputerChoice()));
+    result();
+});
+scissors.addEventListener("click", function () {
+    console.log(playRound("scissors", getComputerChoice()));
+    result();
+});
+
 /*
 !DEPRECATED CODE FOR ALERT VERSION OF GAME
 let game = () => {
