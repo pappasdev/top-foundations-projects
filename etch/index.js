cfad86a5a0d427fbfba5b68f432e041552ee46c8
Gregory Pappas <gnpappas0@gmail.com>
2023-05-16 20:36:56 -1000

    etch-a-sketch completed

---
+++ etch/index.js
@@ -0,0 +1,51 @@
+let container = document.querySelector("#container");
+const containerSize = Math.min(500, 500);
+
+//? this function generates a grid, as you can tell by the name.
+function grid() {
+    //? this can technically work as expected with any number, but I decided to cap it at 50 as a personal preference.
+    let gChoice = prompt("Enter a number 1-50.", "16");
+    if (gChoice > 50) {
+        alert("Value must be under 50. Default grid has been made.");
+        gChoice = 16;
+    }
+    /* 
+    ? add constraints so divs fill in evenly inside the container no matter the user input.
+    ? this was the hardest part to figure out because... math. i did not come up with this formula.
+    ? my own solution worked to a degree, but would go from square to rectangle as the input got bigger
+    */
+    const gridSize = Math.floor(containerSize / gChoice);
+    const containerWidth = gridSize * gChoice;
+    const containerHeight = gridSize * gChoice;
+    container.style.width = `${containerWidth}px`;
+    container.style.height = `${containerHeight}px`;
+    //? standard loop to generate based on user input.
+    for (let i = 0; i < gChoice * gChoice; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${gridSize}px`;  //? based on earlier formula
+        square.style.height = `${gridSize}px`; //? based on earlier formula
+        container.appendChild(square);
+        //? apply new background color to provide painting effect with mouse.
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "#000000";
+        });
+    }
+    gridChoice.disabled = true;
+    reset.disabled = false;
+}
+
+let gridChoice = document.querySelector("#gridChoice");
+gridChoice.addEventListener("click", grid);
+
+//? wrote reset on my own after finally understanding selectorAll (a node list) and forEach.
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        container.removeChild(square);
+    });
+    gridChoice.disabled = false;
+    reset.disabled = true;
+});
+reset.disabled = true;
