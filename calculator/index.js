454a0b223f66e7346a9e50255f236f98a7060c93
Gregory Pappas <gnpappas0@gmail.com>
2023-05-23 16:39:03 -1000

    basic functionality of calculator completed

--- calculator/index.js
+++ calculator/index.js
@@ -1,52 +1,69 @@
 //? calculator functions
-let add = (a,b) => {
+let add = (a, b) => {
     return a + b;
-}
-let subtract = (a,b) => {
+};
+let subtract = (a, b) => {
     return a - b;
-}
+};
 
-let multiply = (a,b) => {
+let multiply = (a, b) => {
     return a * b;
-}
+};
 
-let divide = (a,b) => {
+let divide = (a, b) => {
     return a / b;
-}
+};
 
-//? variables for the calculator operation
-let firstNum;
-let operator;
-let secondNum;
+//? variables to hold values
+var firstNum = 0;
+var oper = "";
+var secondNum = 0;
+var displayValue = "";
+var operationChoice = null;
 
-//? calculate input
-let operate = (first, op, second) => {
-     if (op == '+') {
-       return add(first, second);
-     }
-     if (op == '-') {
-        return subtract(first, second);
-     }
-     if (op == '*') {
-        return multiply(first, second);
-     }
-     if (op == '/') {
-        return divide(first, second);
-     }
-}
-
-console.log(operate(10, '/', 5));
-
-//? buttons input to screen
-let input = document.querySelector('#inputText');
-const buttons = document.querySelectorAll('.input');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        input.textContent = button.textContent;
+//? numbers input to screen
+let input = document.querySelector("#inputText");
+const numbers = document.querySelectorAll(".numbers");
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        input.textContent = number.textContent;
+        displayValue = number.textContent;
+        if (firstNum !== undefined && oper !== undefined) {
+            secondNum = Number(displayValue);
+        }
     });
-})
+});
+
+//? operators assign displayValue to firstNum and chooses what operation to run based on text content
+const operators = document.querySelectorAll(".operators");
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        oper = operator.textContent;
+        firstNum = Number(displayValue);
+    });
+});
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", () => {
+    if (oper === "/") {
+        operationChoice = divide(firstNum, secondNum);
+    }
+    if (oper === "x") {
+        operationChoice = multiply(firstNum, secondNum);
+    }
+    if (oper === "+") {
+        operationChoice = add(firstNum, secondNum);
+    }
+    if (oper === "-") {
+        operationChoice = subtract(firstNum, secondNum);
+    }
+    input.textContent = operationChoice;
+});
+
 //? AC button
-let clear = document.querySelector('#clear');
-clear.addEventListener('click', () => {
-    input.textContent = '0';
-})
+let clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    firstNum = 0;
+    secondNum = 0;
+    input.textContent = "0";
+});
