aa381f0fc046d9a6fd2ee9d49299d5f02daae1b1
Gregory Pappas <gnpappas0@gmail.com>
2023-06-17 18:46:00 -1000

    Add proper functionality. Needs delete button

--- library/index.js
+++ library/index.js
@@ -1,8 +1,6 @@
 let myLibrary = [];
-const button = document.getElementById('bookSubmit');
-const bookDetails = document.getElementById('bookDetails');
-const gridTitles = document.getElementById('titles');
-const book = document.getElementById('book');
+const button = document.getElementById("bookSubmit");
+const bookEntries = document.getElementById("bookEntries");
 
 function Book(author, title, read) {
     this.author = author;
@@ -10,26 +8,44 @@ function Book(author, title, read) {
     this.read = read;
 }
 
-
 function addBookToLibrary(event) {
     event.preventDefault();
-    const authorInput = document.getElementById('author').value;
-    const titleInput = document.getElementById('title').value;
-    const hasRead = document.getElementById('read').value;
-    const newBook = new Book(authorInput, titleInput, hasRead);
+    const authorInput = document.getElementById("author").value;
+    const titleInput = document.getElementById("title").value;
+    const newBook = new Book(authorInput, titleInput, "Unread");
     myLibrary.push(newBook);
     displayBook();
 }
 
 function displayBook() {
-    let entry = ''
+    bookEntries.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].read === 'yes') {
-            entry += `<p>${myLibrary[i].author}</p><p>${myLibrary[i].title}</p><p>Has been read</p>`;
-        } else if (myLibrary[i].read === 'no') {
-            entry += `<p>${myLibrary[i].author}</p><p>${myLibrary[i].title}</p><p>Has not been read</p>`;
-        }
+        const bookButton = document.createElement("button");
+        bookButton.textContent = myLibrary[i].read ? "Unread" : "Read";
+        bookButton.classList.toggle("notRead");
+
+        bookButton.addEventListener("click", function () {
+            if (myLibrary[i].read === "Unread") {
+                myLibrary[i].read = "Read";
+                bookButton.classList.toggle("notRead");
+                bookButton.classList.toggle("isRead");
+                bookButton.textContent = "Read";
+            } else if (myLibrary[i].read === "Read") {
+                myLibrary[i].read = "Unread";
+                bookButton.classList.toggle("isRead");
+                bookButton.classList.toggle("notRead");
+                bookButton.textContent = "Unread";
+            }
+        });
+        const gridTitles = document.createElement('div')
+        gridTitles.classList.toggle('titles');
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.toggle('bookEntry')
+        gridTitles.innerHTML += `<h2>Author</h2><h2>Title</h2><h2>Status</h2>`;
+        bookDiv.innerHTML += `<p>${myLibrary[i].author}</p><p>${myLibrary[i].title}</p>`;
+        bookDiv.appendChild(bookButton);
+        bookEntries.appendChild(gridTitles);
+        bookEntries.appendChild(bookDiv);
     }
-    bookDetails.innerHTML = entry;
 }
-button.addEventListener('click', addBookToLibrary)
+button.addEventListener("click", addBookToLibrary);
